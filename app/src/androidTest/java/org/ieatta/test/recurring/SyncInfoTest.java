6214ddc8bbeafd908c13940795bac19b9ea75076
Trujun Zhang <trujunzhang@gmail.com>
2016-03-12 17:14:49 +0800

    IEATTA for Android.

--- app/src/androidTest/java/org/ieatta/test/recurring/SyncInfoTest.java
+++ app/src/androidTest/java/org/ieatta/test/recurring/SyncInfoTest.java
@@ -5,6 +5,13 @@ import android.support.test.runner.AndroidJUnit4;
 import org.ieatta.server.recurring.SyncInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.wikipedia.settings.Prefs;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by djzhang on 3/12/16.
@@ -12,9 +19,25 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class SyncInfoTest {
 
+    public SimpleDateFormat getSimpleDateFormat() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ROOT);
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return simpleDateFormat;
+    }
+
     @Test
     public void testStoreAndRead() {
-        SyncInfo info;
+        Date lastRunDate = new Date();
+        Prefs.setLastRunTime("sync", lastRunDate.getTime());
+
+        String lastRunDateString = getSimpleDateFormat().format(lastRunDate);
+
+        long lastRunTime = Prefs.getLastRunTime("sync");
+        Date storedDate = new Date(lastRunTime);
+
+        String storedDateString = getSimpleDateFormat().format(storedDate);
+
+        assertThat("equal each other.",lastRunDateString.equals(storedDateString));
     }
 
 }
