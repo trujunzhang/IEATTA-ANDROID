c68bf990f73d041f893cbe8e9d38b5e2e852df1d
Trujun Zhang <trujunzhang@gmail.com>
2016-03-12 19:01:07 +0800

    IEATTA for android.

--- app/src/main/java/org/ieatta/parse/ParseObjectReader.java
+++ app/src/main/java/org/ieatta/parse/ParseObjectReader.java
@@ -1,5 +1,49 @@
 package org.ieatta.parse;
 
 
+import com.parse.ParseObject;
+
+import org.ieatta.database.models.DBEvent;
+import org.ieatta.database.models.DBNewRecord;
+import org.ieatta.database.models.DBPeopleInEvent;
+import org.ieatta.database.models.DBPhoto;
+import org.ieatta.database.models.DBRecipe;
+import org.ieatta.database.models.DBRestaurant;
+import org.ieatta.database.models.DBReview;
+import org.ieatta.database.models.DBTeam;
+
 public class ParseObjectReader {
+
+    public void reader(ParseObject object,DBEvent event){
+
+    }
+
+    public void reader(ParseObject object,DBNewRecord newRecord){
+
+    }
+
+
+    public void reader(ParseObject object,DBPeopleInEvent peopleInEvent){
+
+    }
+
+    public void reader(ParseObject object,DBPhoto photo){
+
+    }
+
+    public void reader(ParseObject object,DBRecipe recipe){
+
+    }
+
+    public void reader(ParseObject object,DBRestaurant restaurant){
+
+    }
+
+    public void reader(ParseObject object,DBReview review){
+
+    }
+
+    public void reader(ParseObject object,DBTeam team){
+
+    }
 }
