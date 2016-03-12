cd1f68750275c407de5a5788703fb33c05a27514
Trujun Zhang <trujunzhang@gmail.com>
2016-03-12 20:05:56 +0800

    IEATTA for android.

--- app/src/main/java/org/ieatta/analytics/ModelsFunnel.java
+++ app/src/main/java/org/ieatta/analytics/ModelsFunnel.java
@@ -3,13 +3,54 @@ package org.ieatta.analytics;
 import org.ieatta.IEATTAApp;
 import org.wikipedia.analytics.Funnel;
 
-/**
- * Created by djzhang on 3/12/16.
- */
-public class ModelsFunnel  extends Funnel {
+import org.ieatta.database.models.DBEvent;
+import org.ieatta.database.models.DBNewRecord;
+import org.ieatta.database.models.DBPeopleInEvent;
+import org.ieatta.database.models.DBPhoto;
+import org.ieatta.database.models.DBRecipe;
+import org.ieatta.database.models.DBRestaurant;
+import org.ieatta.database.models.DBReview;
+import org.ieatta.database.models.DBTeam;
 
+public class ModelsFunnel extends Funnel {
+
+    private static final String SCHEMA_NAME = "MobileIEATTAModels";
+    private static final int REV_ID = 101010;
 
     ModelsFunnel(IEATTAApp app, String schemaName, int revision) {
-        super(app, schemaName, revision);
+        super(app, SCHEMA_NAME, REV_ID, SAMPLE_LOG_1K);
     }
+
+    public void logEvent(DBEvent event){
+
+    }
+
+    public void logNewRecord(DBNewRecord newRecord){
+
+    }
+
+    public void logPeopleInEvent(DBPeopleInEvent peopleInEvent){
+
+    }
+
+    public void logPhoto(DBPhoto photo){
+
+    }
+
+    public void logRecipe(DBRecipe recipe){
+
+    }
+
+    public void logRestaurant(DBRestaurant restaurant){
+
+    }
+
+    public void logReview(DBReview review){
+
+    }
+
+    public void logTeam(DBTeam team){
+
+    }
+
 }
