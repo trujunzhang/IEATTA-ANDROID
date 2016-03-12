bfc878cefc4077570baae9af7f47437a4f0698b9
Trujun Zhang <trujunzhang@gmail.com>
2016-03-12 16:38:10 +0800

    IEATTA for Android.

---
+++ app/src/main/java/org/ieatta/server/sync/SyncNotify.java
@@ -0,0 +1,20 @@
+package org.ieatta.server.sync;
+
+/**
+ * Created by djzhang on 11/30/15.
+ */
+public class SyncNotify {
+
+//    public static void notify(ParseModelAbstract model) {
+//        switch (model.getModelType()) {
+//            case Restaurant:
+//                NSNotificationCenter.defaultCenter().postNotificationName(NotifyType.PAModelCreatedRestaurantNotification, null);
+//                break;
+//            case Review:
+//                break;
+//            default:
+//                break;
+//        }
+//
+//    }
+}
