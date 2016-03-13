e933be7090965111e32a157e87005a05cf4f5269
Trujun Zhang <trujunzhang@gmail.com>
2016-03-13 22:03:35 +0800

    IEATTA for android.

--- app/src/main/java/org/ieatta/tasks/NearRestaurantsTask.java
+++ app/src/main/java/org/ieatta/tasks/NearRestaurantsTask.java
@@ -6,23 +6,28 @@ import android.location.Location;
 import com.parse.ParseGeoPoint;
 
 import org.ieatta.database.models.DBRestaurant;
+import org.ieatta.database.query.LocalDatabaseQuery;
 
 import java.util.List;
 
+import bolts.Continuation;
 import bolts.Task;
 
 public class NearRestaurantsTask {
     private List<DBRestaurant> restaurants;
 
-    public Task<Void> executeTask(Location location){
-        return null;
-    }
-
     public List<DBRestaurant> getRestaurants() {
         return restaurants;
     }
 
-    public void setRestaurants(List<DBRestaurant> restaurants) {
-        this.restaurants = restaurants;
+    public Task<Void> executeTask(Location location){
+        return LocalDatabaseQuery.queryNearRestaurants(location).onSuccess(new Continuation<List<DBRestaurant>, Void>() {
+            @Override
+            public Void then(Task<List<DBRestaurant>> task) throws Exception {
+                NearRestaurantsTask.this.restaurants = task.getResult();
+                return null;
+            }
+        });
     }
+
 }
