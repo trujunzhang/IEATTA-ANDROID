0e133b50ab2c1b4f52bf8b0b650ff5827a8d2a93
Trujun Zhang <trujunzhang@gmail.com>
2016-03-13 21:52:44 +0800

    IEATTA for android.

--- app/src/androidTest/java/org/ieatta/test/query/LocalDatabaseQueryTest.java
+++ app/src/androidTest/java/org/ieatta/test/query/LocalDatabaseQueryTest.java
@@ -3,23 +3,12 @@ package org.ieatta.test.query;
 import android.location.Location;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.github.davidmoten.geo.GeoHash;
-
-import org.ieatta.database.models.DBNewRecord;
 import org.ieatta.database.models.DBRestaurant;
-import org.ieatta.database.provide.PQueryModelType;
 import org.ieatta.database.query.LocalDatabaseQuery;
-import org.ieatta.server.recurring.SyncInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.wikipedia.settings.Prefs;
-import org.wikipedia.testlib.TestLatch;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
-import java.util.TimeZone;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -30,7 +19,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(AndroidJUnit4.class)
 public class LocalDatabaseQueryTest {
-
     private static final int TASK_COMPLETION_TIMEOUT = 20000;
 
     @Test
@@ -39,18 +27,18 @@ public class LocalDatabaseQueryTest {
         Location location = getLocation();
         LocalDatabaseQuery.queryNearRestaurants(location).onSuccess(new Continuation<List<DBRestaurant>, Void>() {
             @Override
-            public Void then(Task<List<DBRestaurant>> task) throws Exception {
+            public Void then(Task<List<DBRestaurant>> task) {
                 List<DBRestaurant> result = task.getResult();
                 assertThat("Fetched restaurants length", (result.size() == 2));
                 completionLatch.countDown();
                 return null;
             }
         });
-        assertThat("",completionLatch.await(TASK_COMPLETION_TIMEOUT, TimeUnit.MILLISECONDS));
+        assertThat("", completionLatch.await(TASK_COMPLETION_TIMEOUT, TimeUnit.MILLISECONDS));
     }
 
     /**
-     * // 'dr5ru0r0y4xj','dr5ru1nwsk1v','dr5ru22mf339','dr5ru0r24xuf','dr5ru22mf339','dr5ru0nzfv5q','wtv8r4whgz2y'
+     * ['dr5ru0r0y4xj','dr5ru1nwsk1v','dr5ru22mf339','dr5ru0r24xuf','dr5ru22mf339','dr5ru0nzfv5q','wtv8r4whgz2y']
      * @return
      */
     private Location getLocation() {
