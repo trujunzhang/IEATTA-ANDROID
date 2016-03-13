6df7de6df9711c53a5bab6d94a7102fd66e96132
Trujun Zhang <trujunzhang@gmail.com>
2016-03-13 20:14:58 +0800

    IEATTA for android.

---
+++ app/src/main/java/org/ieatta/database/realm/DBBuilder.java
@@ -0,0 +1,45 @@
+package org.ieatta.database.realm;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DBBuilder {
+    private HashMap<String, Object> equalMap = new LinkedHashMap<>();
+    private HashMap<String, Object> greaterMap = new LinkedHashMap<>();
+    private HashMap<String, String> matchersMap = new LinkedHashMap<>();
+    public HashMap<String, List<String>> containedMap = new LinkedHashMap<>();
+    private List<String> orderedByDescendingList = new LinkedList<>();
+    private List<String> orderedByAscendingList = new LinkedList<>();
+    public int limit = -1; // negative limits mean, do not send a limit
+
+    public void whereEqualTo(String key, Object value) {
+        this.equalMap.put(key, value);
+    }
+
+    public void whereGreaterThan(String key, Object value) {
+        this.greaterMap.put(key, value);
+    }
+
+    public void whereContainedIn(String key, List<String> list) {
+        this.containedMap.put(key, list);
+    }
+
+    public void whereMatchers(String key, String keyword) {
+        this.matchersMap.put(key, keyword);
+    }
+
+    public void orderByDescending(String key) {
+        this.orderedByDescendingList.add(key);
+    }
+
+    public void orderByAscending(String key) {
+        this.orderedByAscendingList.add(key);
+    }
+
+    public void setLimit(int newLimit) {
+        this.limit = newLimit;
+    }
+
+}
