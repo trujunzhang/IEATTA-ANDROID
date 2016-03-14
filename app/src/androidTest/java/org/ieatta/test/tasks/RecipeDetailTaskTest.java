eeefed28e2dcc9d081d0da34f709bb9fa2689067
Trujun Zhang <trujunzhang@gmail.com>
2016-03-14 10:31:58 +0800

    IEATTA for android.

---
+++ app/src/androidTest/java/org/ieatta/test/tasks/RecipeDetailTaskTest.java
@@ -0,0 +1,37 @@
+package org.ieatta.test.tasks;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.ieatta.tasks.OrderedRecipesTask;
+import org.ieatta.tasks.RecipeDetailTask;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import bolts.Continuation;
+import bolts.Task;
+
+@RunWith(AndroidJUnit4.class)
+public class RecipeDetailTaskTest {
+    private static final int TASK_COMPLETION_TIMEOUT = 20000;
+    private RecipeDetailTask task = new RecipeDetailTask();
+    private String restaurantUUID = "1CE562A4-A978-4B75-9B7B-2F3CF9F42A04";
+    private String eventUUID = "07B2D33C-F11D-404B-9D78-016D16BEE9FE";
+    private String teamUUID = "5C52C84D-4E0B-4727-B3FD-274C9F004580";
+    private String recipeUUID = "";
+
+    @Test
+    public void testOrderedRecipes() throws InterruptedException {
+        final CountDownLatch completionLatch = new CountDownLatch(1);
+        task.executeTask(restaurantUUID,eventUUID,teamUUID,recipeUUID).onSuccess(new Continuation<Void, Void>() {
+            @Override
+            public Void then(Task<Void> task) throws Exception {
+                completionLatch.countDown();
+                return null;
+            }
+        });
+        completionLatch.await(TASK_COMPLETION_TIMEOUT, TimeUnit.MILLISECONDS);
+    }
+}
---
+++ app/src/main/java/org/ieatta/tasks/RecipeDetailTask.java
@@ -0,0 +1,62 @@
+package org.ieatta.tasks;
+
+import org.ieatta.database.models.DBEvent;
+import org.ieatta.database.models.DBPhoto;
+import org.ieatta.database.models.DBRecipe;
+import org.ieatta.database.models.DBRestaurant;
+import org.ieatta.database.models.DBReview;
+import org.ieatta.database.models.DBTeam;
+import org.ieatta.database.query.LocalDatabaseQuery;
+import org.ieatta.database.realm.RealmModelReader;
+
+import bolts.Continuation;
+import bolts.Task;
+import io.realm.RealmResults;
+
+public class RecipeDetailTask {
+    public DBRestaurant restaurant;
+    public DBEvent event;
+    public DBTeam team;
+    public DBRecipe recipe;
+    public RealmResults<DBReview> reviews;
+    public RealmResults<DBPhoto> galleryCollection;
+
+    /**
+     * Execute Task for Restaurant detail.
+     *
+     * @param restaurantUUID restaurant's UUID
+     * @param eventUUID      event's UUID
+     * @param teamUUID       team's UUID
+     * @return
+     */
+    public Task<Void> executeTask(final String restaurantUUID, final String eventUUID, final String teamUUID, final String recipeUUID) {
+        return new RealmModelReader<DBRestaurant>(DBRestaurant.class).getFirstObject(LocalDatabaseQuery.get(restaurantUUID), false)
+                .onSuccessTask(new Continuation<DBRestaurant, Task<DBEvent>>() {
+                    @Override
+                    public Task<DBEvent> then(Task<DBRestaurant> task) throws Exception {
+                        RecipeDetailTask.this.restaurant = task.getResult();
+                        return new RealmModelReader<DBEvent>(DBEvent.class).getFirstObject(LocalDatabaseQuery.get(eventUUID), false);
+                    }
+                })
+                .onSuccessTask(new Continuation<DBEvent, Task<DBTeam>>() {
+                    @Override
+                    public Task<DBTeam> then(Task<DBEvent> task) throws Exception {
+                        RecipeDetailTask.this.event = task.getResult();
+                        return new RealmModelReader<DBTeam>(DBTeam.class).getFirstObject(LocalDatabaseQuery.get(teamUUID), false);
+                    }
+                })
+                .onSuccessTask(new Continuation<DBTeam, Task<DBRecipe>>() {
+                    @Override
+                    public Task<DBRecipe> then(Task<DBTeam> task) throws Exception {
+                        RecipeDetailTask.this.team = task.getResult();
+                        return new RealmModelReader<DBRecipe>(DBRecipe.class).getFirstObject(LocalDatabaseQuery.get(recipeUUID), false);
+                    }
+                }).onSuccess(new Continuation<DBRecipe, Void>() {
+                    @Override
+                    public Void then(Task<DBRecipe> task) throws Exception {
+                        RecipeDetailTask.this.recipe = task.getResult();
+                        return null;
+                    }
+                });
+    }
+}
