b5eb92799844f9c04bcdc417329dc02200c7cfde
Trujun Zhang <trujunzhang@gmail.com>
2016-03-14 09:37:14 +0800

    IEATTA for android.

--- app/src/androidTest/java/org/ieatta/test/tasks/RestaurantDetailTaskTest.java
+++ app/src/androidTest/java/org/ieatta/test/tasks/RestaurantDetailTaskTest.java
@@ -1,16 +1,11 @@
 package org.ieatta.test.tasks;
 
-import android.location.Location;
-import android.print.PageRange;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.ieatta.database.models.DBRestaurant;
-import org.ieatta.database.query.LocalDatabaseQuery;
 import org.ieatta.tasks.RestaurantDetailTask;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -23,12 +18,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class RestaurantDetailTaskTest {
     private static final int TASK_COMPLETION_TIMEOUT = 20000;
     private RestaurantDetailTask task = new RestaurantDetailTask();
-    private String UUID = "1CE562A4-A978-4B75-9B7B-2F3CF9F42A04";
+    private String restaurantUUID = "1CE562A4-A978-4B75-9B7B-2F3CF9F42A04";
 
     @Test
     public void testRestaurantDetail() throws InterruptedException {
         final CountDownLatch completionLatch = new CountDownLatch(1);
-        task.executeTask(UUID).onSuccess(new Continuation<Void, Void>() {
+        task.executeTask(restaurantUUID).onSuccess(new Continuation<Void, Void>() {
             @Override
             public Void then(Task<Void> task) throws Exception {
                 completionLatch.countDown();
--- app/src/main/java/org/ieatta/tasks/EventDetailTask.java
+++ app/src/main/java/org/ieatta/tasks/EventDetailTask.java
@@ -4,6 +4,7 @@ import org.ieatta.database.models.DBEvent;
 import org.ieatta.database.models.DBPhoto;
 import org.ieatta.database.models.DBRestaurant;
 import org.ieatta.database.models.DBReview;
+import org.ieatta.database.models.DBTeam;
 import org.ieatta.database.provide.ReviewType;
 import org.ieatta.database.query.LocalDatabaseQuery;
 import org.ieatta.database.realm.DBBuilder;
@@ -14,44 +15,37 @@ import bolts.Continuation;
 import bolts.Task;
 import io.realm.RealmResults;
 
-public class RestaurantDetailTask {
+public class EventDetailTask {
     public DBRestaurant restaurant;
-    public RealmResults<DBEvent> events;
+    public DBEvent event;
+    public RealmResults<DBTeam> teams;
     public RealmResults<DBReview> reviews;
-    public RealmResults<DBPhoto> galleryCollection;
 
     /**
      * Execute Task for Restaurant detail.
      *
-     * @param restaurantUUID restaurant's UUID
+     * @param UUID restaurant's UUID
      * @return
      */
-    public Task<Void> executeTask(final String restaurantUUID) {
-        return new RealmModelReader<DBRestaurant>(DBRestaurant.class).getFirstObject(LocalDatabaseQuery.get(restaurantUUID), false).onSuccessTask(new Continuation<DBRestaurant, Task<RealmResults<DBPhoto>>>() {
+    public Task<Void> executeTask(final String UUID) {
+        return new RealmModelReader<DBRestaurant>(DBRestaurant.class).getFirstObject(LocalDatabaseQuery.get(UUID), false).onSuccessTask(new Continuation<DBRestaurant, Task<RealmResults<DBEvent>>>() {
             @Override
-            public Task<RealmResults<DBPhoto>> then(Task<DBRestaurant> task) throws Exception {
-                RestaurantDetailTask.this.restaurant = task.getResult();
-                return LocalDatabaseQuery.queryPhotosForRestaurant(restaurantUUID);
-            }
-        }).onSuccessTask(new Continuation<RealmResults<DBPhoto>, Task<RealmResults<DBEvent>>>() {
-            @Override
-            public Task<RealmResults<DBEvent>> then(Task<RealmResults<DBPhoto>> task) throws Exception {
-                RestaurantDetailTask.this.galleryCollection = task.getResult();
+            public Task<RealmResults<DBEvent>> then(Task<DBRestaurant> task) throws Exception {
+                EventDetailTask.this.restaurant = task.getResult();
                 return new RealmModelReader<DBEvent>(DBEvent.class).fetchResults(
-                        new DBBuilder().whereEqualTo(DBConstant.kPAPFieldLocalRestaurantKey, restaurantUUID), false);
+                        new DBBuilder().whereEqualTo(DBConstant.kPAPFieldLocalRestaurantKey, UUID), false);
             }
         }).onSuccessTask(new Continuation<RealmResults<DBEvent>, Task<RealmResults<DBReview>>>() {
             @Override
             public Task<RealmResults<DBReview>> then(Task<RealmResults<DBEvent>> task) throws Exception {
-                RestaurantDetailTask.this.events = task.getResult();
                 return new RealmModelReader<DBReview>(DBReview.class).fetchResults(
-                        new DBBuilder().whereEqualTo(DBConstant.kPAPFieldReviewRefKey, restaurantUUID)
+                        new DBBuilder().whereEqualTo(DBConstant.kPAPFieldReviewRefKey, UUID)
                                 .whereEqualTo(DBConstant.kPAPFieldReviewTypeKey, ReviewType.Review_Restaurant.getType()), false);
             }
         }).onSuccess(new Continuation<RealmResults<DBReview>, Void>() {
             @Override
             public Void then(Task<RealmResults<DBReview>> task) throws Exception {
-                RestaurantDetailTask.this.reviews = task.getResult();
+                EventDetailTask.this.reviews = task.getResult();
                 return null;
             }
         });
