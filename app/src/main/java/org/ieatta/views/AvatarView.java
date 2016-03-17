302936fd3fb0af15b74088481d6129ceaf729b15
Trujun Zhang <trujunzhang@gmail.com>
2016-03-17 18:22:57 +0800

    IEATTA for Android.

---
+++ app/src/main/java/org/ieatta/views/AvatarView.java
@@ -0,0 +1,23 @@
+package org.ieatta.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class AvatarView extends ImageView{
+    public AvatarView(Context context) {
+        super(context);
+    }
+
+    public AvatarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AvatarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public AvatarView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
