1f9ca4b984c25702d6cf35502cf23a00e750b81e
Trujun Zhang <trujunzhang@gmail.com>
2016-03-17 10:51:23 +0800

    NearRestaurantsActivity.

--- app/src/main/java/org/ieatta/activity/fragments/NearRestaurantsFragment.java
+++ app/src/main/java/org/ieatta/activity/fragments/NearRestaurantsFragment.java
@@ -23,4 +23,34 @@ public class NearRestaurantsFragment extends IEAFragment {
 
         return view;
     }
+
+
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_near_restaurants);
+//
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+//        manager = new RecycleViewManager(this.getApplicationContext());
+//        this.setupUI();
+//        this.manager.setSectionItems(IEANearRestaurantMore.getMoresItems(), NearRestaurantSection.sectionMoreItems.ordinal());
+//    }
+
+//    private void setupUI() {
+//        this.manager.setRegisterCellClassWhenSelected(IEANearRestaurantsCell.getType(), NearRestaurantSection.sectionRestaurants.ordinal());
+//        this.manager.setRegisterCellClassWhenSelected(IEANearRestaurantMoreCell.getType(), NearRestaurantSection.sectionMoreItems.ordinal());
+//
+//        this.manager.appendSectionTitleCell(new SectionTitleCellModel(IEAEditKey.Section_Title, R.string.More), NearRestaurantSection.sectionMoreItems.ordinal());
+//    }
 }
