0fdfe5814ef40f580607724a9d2965afdaf7d21f
Trujun Zhang <trujunzhang@gmail.com>
2016-03-18 18:17:06 +0800

    IEATTA

--- app/src/androidTest/java/org/ieatta/test/query/LocalDatabaseQueryTest.java
+++ app/src/androidTest/java/org/ieatta/test/query/LocalDatabaseQueryTest.java
@@ -3,6 +3,7 @@ package org.ieatta.test.query;
 import android.location.Location;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.ieatta.database.models.DBPhoto;
 import org.ieatta.database.models.DBRestaurant;
 import org.ieatta.database.query.LocalDatabaseQuery;
 import org.ieatta.utils.LocationUtil;
@@ -24,7 +25,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class LocalDatabaseQueryTest {
     private static final int TASK_COMPLETION_TIMEOUT = 20000;
 
-
     @Test
     public void testQueryNearRestaurants() throws InterruptedException {
         final CountDownLatch completionLatch = new CountDownLatch(1);
@@ -43,6 +43,22 @@ public class LocalDatabaseQueryTest {
         completionLatch.await(TASK_COMPLETION_TIMEOUT, TimeUnit.MILLISECONDS);
     }
 
+    @Test
+    public void testQueryPhoto() throws InterruptedException {
+        final String usedRef = "C2F23EDC-106C-4D17-A6D6-8EA04E10732A"; // for Restaurant(called "Basta Pasta").
+        final CountDownLatch completionLatch = new CountDownLatch(1);
 
-
+        LocalDatabaseQuery.getPhoto(usedRef).onSuccess(new Continuation<DBPhoto, Object>() {
+            @Override
+            public Object then(Task<DBPhoto> task) throws Exception {
+                DBPhoto photo = task.getResult();
+                String expect = photo.getUsedRef();
+                L.d("usedRef of the photo: " + expect);
+                assertThat("Fetched photo's usedRef", (usedRef == expect));
+                completionLatch.countDown();
+                return null;
+            }
+        });
+        completionLatch.await(TASK_COMPLETION_TIMEOUT, TimeUnit.MILLISECONDS);
+    }
 }
