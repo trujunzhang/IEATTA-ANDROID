cc04be07b0bd30451e328d7df0d89775d14c00a3
Trujun Zhang <trujunzhang@gmail.com>
2016-03-18 18:04:37 +0800

    IEATTA

--- app/src/main/java/org/ieatta/views/AvatarView.java
+++ app/src/main/java/org/ieatta/views/AvatarView.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
-import org.ieatta.database.models.DBRestaurant;
+import com.facebook.drawee.view.SimpleDraweeView;
 
-public class AvatarView extends ImageView{
+import org.ieatta.database.models.DBRestaurant;
+import org.wikipedia.views.ViewUtil;
+
+public class AvatarView extends SimpleDraweeView {
     public AvatarView(Context context) {
         super(context);
     }
@@ -23,7 +26,26 @@ public class AvatarView extends ImageView{
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
-    public void loadNewPhotoByModel(DBRestaurant model, int blank_biz) {
+    public void loadNewPhotoByModel(DBRestaurant model, int placeHolder) {
+        this.configureAvatar(placeHolder);
 
+
+
+//        new DBPhoto().queryPhotosByModel(model)
+//                .onSuccess(new Continuation<List<ParseModelAbstract>, Void>() {
+//                    @Override
+//                    public Void then(Task<List<ParseModelAbstract>> task) throws Exception {
+//                        List<ParseModelAbstract> taskResult = task.getResult();
+//                        ParseModelAbstract first = taskResult.get(0);
+//                        if (first != null) {
+//                            self.loadNewPhotoByPhoto((Photo) first, placeHolder);
+//                        }
+//                        return null;
+//                    }
+//                });
+    }
+
+    private void configureAvatar(int placeHolder) {
+//        ViewUtil.loadImageUrlInto(this, mGalleryItem.getThumbUrl());
     }
 }
