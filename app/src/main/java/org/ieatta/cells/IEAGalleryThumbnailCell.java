30c4353b5293a83c6dc1ec9a45d15e1eaf599aef
Trujun Zhang <trujunzhang@gmail.com>
2016-03-20 13:58:46 +0800

    IEATTA

--- app/src/main/java/org/ieatta/cells/IEAGalleryThumbnailCell.java
+++ app/src/main/java/org/ieatta/cells/IEAGalleryThumbnailCell.java
@@ -8,6 +8,8 @@ import com.tableview.adapter.IEAViewHolder;
 import com.tableview.storage.models.CellType;
 
 import org.ieatta.R;
+import org.ieatta.activity.gallery.GalleryCollection;
+import org.ieatta.activity.gallery.GalleryThumbnailScrollView;
 import org.ieatta.cells.model.IEANearRestaurantMore;
 
 public class IEAGalleryThumbnailCell extends IEAViewHolder {
@@ -15,14 +17,32 @@ public class IEAGalleryThumbnailCell extends IEAViewHolder {
         return new CellType(IEAGalleryThumbnailCell.class, R.layout.cell_gallerythumbnail);
     }
 
+    private GalleryThumbnailScrollView thumbnailGallery;
 
     public IEAGalleryThumbnailCell(View itemView) {
         super(itemView);
 
+        thumbnailGallery = (GalleryThumbnailScrollView) itemView.findViewById(R.id.link_preview_thumbnail_gallery);
     }
 
     @Override
     public void render(Object value) {
+        GalleryCollection result = new GalleryCollection(null);
+        this.setGalleryResult(result);
+    }
 
+    private void setGalleryResult(GalleryCollection result) {
+        if (result.getItemList().size() > 2) {
+            thumbnailGallery.setGalleryCollection(result);
+
+            // When the visibility is immediately changed, the images flicker. Add a short delay.
+            final int animationDelayMillis = 100;
+            thumbnailGallery.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    thumbnailGallery.setVisibility(View.VISIBLE);
+                }
+            }, animationDelayMillis);
+        }
     }
 }
