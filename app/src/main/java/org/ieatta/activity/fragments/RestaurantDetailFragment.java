be2b67943a0f2b5853624c3f4eaa3d741083a27e
Trujun Zhang <trujunzhang@gmail.com>
2016-03-20 14:26:49 +0800

    IEATTA

--- app/src/main/java/org/ieatta/activity/fragments/RestaurantDetailFragment.java
+++ app/src/main/java/org/ieatta/activity/fragments/RestaurantDetailFragment.java
@@ -1,52 +1,22 @@
 package org.ieatta.activity.fragments;
 
-import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.tableview.RecycleViewManager;
 import com.tableview.adapter.NSIndexPath;
 import com.tableview.adapter.RecyclerOnItemClickListener;
 import com.tableview.utils.CollectionUtil;
 
-import org.ieatta.IEAApp;
 import org.ieatta.R;
-import org.ieatta.activity.DetailPageLoadStrategy;
-import org.ieatta.activity.JsonPageLoadStrategy;
-import org.ieatta.activity.Page;
-import org.ieatta.activity.PageActivity;
-import org.ieatta.activity.PageBackStackItem;
-import org.ieatta.activity.PageLoadStrategy;
-import org.ieatta.activity.PageViewModel;
-import org.ieatta.activity.editing.EditHandler;
-import org.ieatta.activity.fragments.search.SearchBarHideHandler;
-import org.ieatta.activity.leadimages.ArticleHeaderView;
-import org.ieatta.activity.leadimages.LeadImagesHandler;
+import org.ieatta.cells.IEAGalleryThumbnailCell;
 import org.ieatta.cells.IEARestaurantEventsCell;
 import org.ieatta.cells.header.IEAEmptyHeaderCell;
-import org.ieatta.cells.header.IEARestaurantDetailHeaderCell;
 import org.ieatta.cells.model.IEAEmptyHeader;
-import org.ieatta.cells.model.IEARestaurantDetailHeader;
+import org.ieatta.cells.model.IEAGalleryThumbnail;
 import org.ieatta.cells.model.SectionTitleCellModel;
-import org.ieatta.database.models.DBEvent;
 import org.ieatta.provide.IEAEditKey;
 import org.ieatta.tasks.RestaurantDetailTask;
-import org.ieatta.views.ObservableWebView;
-import org.wikipedia.analytics.PageScrollFunnel;
-import org.wikipedia.analytics.SavedPagesFunnel;
-import org.wikipedia.analytics.TabFunnel;
-import org.wikipedia.util.DimenUtil;
-import org.wikipedia.views.SwipeRefreshLayoutWithScroll;
-import org.wikipedia.views.WikiDrawerLayout;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 import bolts.Continuation;
 import bolts.Task;
@@ -70,7 +40,7 @@ public class RestaurantDetailFragment extends DetailFragment {
     enum RestaurantDetailSection {
         section_leadimage,
         section_events,//= 0
-        section_photogallery,//= 1
+        section_gallery_thumbnail,//= 1
         section_reviews,//= 2
     }
 
@@ -88,6 +58,7 @@ public class RestaurantDetailFragment extends DetailFragment {
     private void setupUI() {
         this.manager.setRegisterCellClass(IEAEmptyHeaderCell.getType(), RestaurantDetailSection.section_leadimage.ordinal());
         this.manager.setRegisterCellClass(IEARestaurantEventsCell.getType(), RestaurantDetailSection.section_events.ordinal());
+        this.manager.setRegisterCellClass(IEAGalleryThumbnailCell.getType(), RestaurantDetailSection.section_gallery_thumbnail.ordinal());
 
         this.manager.appendSectionTitleCell(new SectionTitleCellModel(IEAEditKey.Section_Title, R.string.Events_Recorded), RestaurantDetailSection.section_events.ordinal());
     }
@@ -119,6 +90,8 @@ public class RestaurantDetailFragment extends DetailFragment {
     protected void reloadPage() {
         this.manager.setSectionItems(CollectionUtil.createList(new IEAEmptyHeader(this.getScreenHeight())), RestaurantDetailSection.section_leadimage.ordinal());
         this.manager.setSectionItems(task.events, RestaurantDetailSection.section_events.ordinal());
+
+        this.manager.setSectionItems(CollectionUtil.createList(new IEAGalleryThumbnail(this.task.galleryPhoto)), RestaurantDetailSection.section_gallery_thumbnail.ordinal());
 
         model.setPage(task.getPage());
 
--- app/src/main/java/org/ieatta/cells/model/IEAGalleryThumbnail.java
+++ app/src/main/java/org/ieatta/cells/model/IEAGalleryThumbnail.java
@@ -1,12 +1,27 @@
 package org.ieatta.cells.model;
 
+import org.ieatta.activity.PageTitle;
 import org.ieatta.activity.gallery.GalleryCollection;
+import org.ieatta.activity.gallery.GalleryItem;
+import org.ieatta.database.models.DBPhoto;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import io.realm.RealmResults;
 
 public class IEAGalleryThumbnail {
     private GalleryCollection result;
 
     public IEAGalleryThumbnail(GalleryCollection result) {
+        Map<PageTitle, GalleryItem> galleryMap = new LinkedHashMap<>();
         this.result = result;
+    }
+
+    public IEAGalleryThumbnail(RealmResults<DBPhoto> galleryPhoto) {
+        Map<PageTitle, GalleryItem> galleryMap = new LinkedHashMap<>();
+        this.result = new GalleryCollection(galleryMap);
     }
 
     public GalleryCollection getResult() {
