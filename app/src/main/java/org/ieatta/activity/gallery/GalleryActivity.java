50fcd7b29cbefefae3a0aa58bf609df71f9a0d1c
Trujun Zhang <trujunzhang@gmail.com>
2016-03-20 20:27:16 +0800

    IEATTA

--- app/src/main/java/org/ieatta/activity/gallery/GalleryActivity.java
+++ app/src/main/java/org/ieatta/activity/gallery/GalleryActivity.java
@@ -130,7 +130,7 @@ public class GalleryActivity extends ThemedActionBarActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // force the theme to dark...
-        setTheme(Theme.DARK.getResourceId());
+//        setTheme(Theme.LIGHT.getResourceId());
         app = (IEAApp) getApplicationContext();
 
         setContentView(R.layout.activity_gallery);
