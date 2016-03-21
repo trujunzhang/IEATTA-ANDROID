2fbaa6a74e836c2fe8caef0ffcca329a2dad42b2
Trujun Zhang <trujunzhang@gmail.com>
2016-03-22 00:17:32 +0800

    IEATTA

--- app/src/main/java/com/tableview/storage/SectionModel.java
+++ app/src/main/java/com/tableview/storage/SectionModel.java
@@ -102,20 +102,4 @@ public class SectionModel {
     public FooterModel getFooterModel() {
         return footerModel;
     }
-
-    public boolean haveHeaderView(){
-        return this.headerModel!= null;
-    }
-
-    public boolean haveFooterView(){
-        return  this.footerModel != null;
-    }
-
-    public RowModel getHeaderViewModel(){
-        return new RowModel(this.headerModel);
-    }
-
-    public RowModel getFooterViewModel(){
-        return new RowModel(this.footerModel);
-    }
 }
