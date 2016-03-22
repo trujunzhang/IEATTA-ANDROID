168648ac38e25ecc7b2d11a5539837bd51e4f909
Trujun Zhang <trujunzhang@gmail.com>
2016-03-22 08:33:09 +0800

    IEATTA

--- app/src/main/java/org/ieatta/database/realm/RealmModelReader.java
+++ app/src/main/java/org/ieatta/database/realm/RealmModelReader.java
@@ -14,13 +14,13 @@ import io.realm.RealmResults;
 import io.realm.Sort;
 
 public class RealmModelReader<T extends RealmObject> {
-    private Class<T > clazz;
+    private Class<T> clazz;
 
     public RealmModelReader(Class<T> clazz) {
         this.clazz = clazz;
     }
 
-    public Task<RealmResults<T>> fetchResults(DBBuilder builder, boolean needClose){
+    public Task<RealmResults<T>> fetchResults(DBBuilder builder, boolean needClose) {
         RealmResults<T> result = null;
 
         Realm realm = Realm.getInstance(IEAApp.getInstance());
@@ -30,13 +30,14 @@ public class RealmModelReader<T extends RealmObject> {
             buildAll(builder, query);
 
             // Execute the query:
-            result= query.findAll();
+            result = query.findAll();
 
-            resultSorted(builder, result);
+            if (result != null)
+                resultSorted(builder, result);
         } catch (Exception e) {
             return Task.forError(e);
         } finally {
-            if(needClose) {
+            if (needClose) {
                 realm.close();
             }
         }
@@ -44,7 +45,7 @@ public class RealmModelReader<T extends RealmObject> {
         return Task.forResult(result);
     }
 
-    public Task<T> getFirstObject(DBBuilder builder, boolean needClose){
+    public Task<T> getFirstObject(DBBuilder builder, boolean needClose) {
         T result = null;
 
         Realm realm = Realm.getInstance(IEAApp.getInstance());
@@ -54,12 +55,12 @@ public class RealmModelReader<T extends RealmObject> {
             buildAll(builder, query);
 
             // Execute the query:
-            result= query.findFirst();
+            result = query.findFirst();
 
         } catch (Exception e) {
             return Task.forError(e);
         } finally {
-            if(needClose) {
+            if (needClose) {
                 realm.close();
             }
         }
@@ -77,63 +78,63 @@ public class RealmModelReader<T extends RealmObject> {
 
     private void buildContainedListMap(DBBuilder builder, RealmQuery<T> query) {
         HashMap<String, List<String>> containedListMap = builder.containedListMap;
-        if(containedListMap.keySet().size() == 0)
+        if (containedListMap.keySet().size() == 0)
             return;
 
-        for(String key : containedListMap.keySet()) {
+        for (String key : containedListMap.keySet()) {
             List<String> list = containedListMap.get(key);
             RealmQuery<T> beginGroup = query.beginGroup();
-            for(String value: list){
-                beginGroup.equalTo(key,value).or();
+            for (String value : list) {
+                beginGroup.equalTo(key, value).or();
             }
             query.endGroup();
         }
     }
 
     private void resultSorted(DBBuilder builder, RealmResults<T> result) {
-        for(String value :builder.orderedByAscendingList) {
+        for (String value : builder.orderedByAscendingList) {
             result.sort(value, Sort.ASCENDING);
         }
-        for(String value :builder.orderedByDescendingList) {
-            result.sort(value,Sort.DESCENDING);
+        for (String value : builder.orderedByDescendingList) {
+            result.sort(value, Sort.DESCENDING);
         }
     }
 
     private void buildEqualMap(DBBuilder builder, RealmQuery<T> query) {
         HashMap<String, Object> equalMap = builder.equalMap;
-        if(equalMap.keySet().size() == 0)
+        if (equalMap.keySet().size() == 0)
             return;
 
-        for(String key : equalMap.keySet()) {
+        for (String key : equalMap.keySet()) {
             Object value = equalMap.get(key);
             if (value instanceof String) {
-                query.equalTo(key, (String)value);
-            }else if (value instanceof Integer) {
-                query.equalTo(key, (int)value);
+                query.equalTo(key, (String) value);
+            } else if (value instanceof Integer) {
+                query.equalTo(key, (int) value);
             }
         }
     }
 
     private void buildContainedMap(DBBuilder builder, RealmQuery<T> query) {
         HashMap<String, String> containedMap = builder.containedMap;
-        if(containedMap.keySet().size() == 0)
+        if (containedMap.keySet().size() == 0)
             return;
 
-        for(String key : containedMap.keySet()) {
+        for (String key : containedMap.keySet()) {
             query.contains(key, containedMap.get(key));
         }
     }
 
     private void buildGreaterMap(DBBuilder builder, RealmQuery<T> query) {
         HashMap<String, Object> greaterMap = builder.greaterMap;
-        if(greaterMap.keySet().size() == 0)
+        if (greaterMap.keySet().size() == 0)
             return;
 
-        for(String key : greaterMap.keySet()) {
+        for (String key : greaterMap.keySet()) {
             Object value = greaterMap.get(key);
             if (value instanceof Date) {
                 query.greaterThan(key, (Date) value);
-            }else if (value instanceof Integer) {
+            } else if (value instanceof Integer) {
                 query.greaterThan(key, (int) value);
             }
         }
