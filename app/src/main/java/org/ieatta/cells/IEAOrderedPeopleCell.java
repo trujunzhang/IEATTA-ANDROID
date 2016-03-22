4cb75cddd0a1e180b9c8478c9a03bbb16b332dc7
Trujun Zhang <trujunzhang@gmail.com>
2016-03-22 09:40:40 +0800

    IEATTA

--- app/src/main/java/org/ieatta/cells/IEAOrderedPeopleCell.java
+++ app/src/main/java/org/ieatta/cells/IEAOrderedPeopleCell.java
@@ -6,9 +6,14 @@ import android.widget.TextView;
 import com.tableview.adapter.IEAViewHolder;
 import com.tableview.storage.models.CellType;
 
+import org.ieatta.IEAApp;
 import org.ieatta.R;
 import org.ieatta.cells.model.IEAOrderedPeople;
+import org.ieatta.database.query.RecipeQuery;
 import org.ieatta.views.AvatarView;
+
+import bolts.Continuation;
+import bolts.Task;
 
 
 public class IEAOrderedPeopleCell extends IEAViewHolder {
@@ -35,6 +40,14 @@ public class IEAOrderedPeopleCell extends IEAViewHolder {
         this.nameLabel.setText(model.getDisplayName());
         this.avatarView.loadNewPhotoByModel(model.getTeamUUID());
 
+        new RecipeQuery().queryOrderedRecipesCount(model.getTeamUUID(),model.getEventUUID()).onSuccess(new Continuation<Long, Void>() {
+            @Override
+            public Void then(Task<Long> task) throws Exception {
+                String info = String.format("%d %s", task.getResult(), IEAApp.getInstance().getResources().getString(R.string.Recipes_Ordered_Count));
+                IEAOrderedPeopleCell.this.recipesCountLabel.setText(info);
+                return null;
+            }
+        });
 
     }
 }
