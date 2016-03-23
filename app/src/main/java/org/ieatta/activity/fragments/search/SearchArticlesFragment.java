d9edad34e26afacee2974cb0086bfc607cecedb0
Trujun Zhang <trujunzhang@gmail.com>
2016-03-23 09:01:43 +0800

    IEATTA

--- app/src/main/java/org/ieatta/activity/fragments/search/SearchArticlesFragment.java
+++ app/src/main/java/org/ieatta/activity/fragments/search/SearchArticlesFragment.java
@@ -2,6 +2,79 @@ package org.ieatta.activity.fragments.search;
 
 import org.ieatta.activity.fragments.PageFragment;
 
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ActionMode;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.ieatta.R;
+import org.wikipedia.BackPressedHandler;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ActionMode;
+import android.text.Html;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.wikipedia.BackPressedHandler;
+import org.wikipedia.Site;
+import org.wikipedia.analytics.FindInPageFunnel;
+import org.wikipedia.analytics.LinkPreviewFunnel;
+import org.wikipedia.analytics.PageScrollFunnel;
+import org.wikipedia.analytics.SavedPagesFunnel;
+import org.wikipedia.analytics.TabFunnel;
+import org.wikipedia.settings.Prefs;
+import org.wikipedia.util.FeedbackUtil;
+import org.wikipedia.util.ThrowableUtil;
+import org.wikipedia.util.log.L;
+import org.wikipedia.views.SwipeRefreshLayoutWithScroll;
+import org.wikipedia.views.WikiDrawerLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.wikipedia.util.DeviceUtil.hideSoftKeyboard;
+import static org.wikipedia.util.DimenUtil.getContentTopOffset;
+import static org.wikipedia.util.DimenUtil.getContentTopOffsetPx;
+import static org.wikipedia.util.ResourceUtil.getThemedAttributeId;
+import static org.wikipedia.util.UriUtil.decodeURL;
+import static org.wikipedia.util.UriUtil.visitInExternalBrowser;
+
 public class SearchArticlesFragment extends PageFragment {
 
     public boolean isSearchActive() {
@@ -11,6 +84,16 @@ public class SearchArticlesFragment extends PageFragment {
     @Override
     public void loadPage() {
 
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.fragment_recycleview, container, false);
+
+        return view;
     }
 
     @Override
