e282792a7ac69961a90f7f345cbb30be49cf101f
Trujun Zhang <trujunzhang@gmail.com>
2016-03-24 17:01:15 +0800

    IEATTA for Android.

--- app/src/main/java/org/ieatta/activity/PageActivity.java
+++ app/src/main/java/org/ieatta/activity/PageActivity.java
@@ -515,13 +515,15 @@ public class PageActivity extends ThemedActionBarActivity {
     }
 
     private void loadMainPageIfNoTabs() {
-//        PageFragment fragment = new NearRestaurantsFragment();
-//        PageFragment fragment = new RestaurantDetailFragment();
-//        PageFragment fragment = new EventDetailFragment();
-//        PageFragment fragment = new OrderedRecipesFragment();
+        String restaurantUUID = "1CE562A4-A978-4B75-9B7B-2F3CF9F42A04"; // The Flying Falafel
+//        String restaurantUUID = "33ED9F31-F6A5-43A4-8D11-8E511CA0BD39"; // The Spice Jar
+        String eventUUID = "07B2D33C-F11D-404B-9D78-016D16BEE9FE"; // White Truffies
+        String teamUUID = "197C0BEF-B432-47B8-988B-99406643623A";// Dolores Chavez
+        String recipeUUID = "95B62D6F-87DF-47E2-8C84-EADAE131BB5D"; // Dark Gelate
 
-        HistoryEntry entry = new HistoryEntry(MainSegueIdentifier.nearbyRestaurants, LocationUtil.getLocation());
-//        HistoryEntry entry =new HistoryEntry(MainSegueIdentifier.detailRestaurantSegueIdentifier);
+
+//        HistoryEntry entry = new HistoryEntry(MainSegueIdentifier.nearbyRestaurants, LocationUtil.getLocation());
+        HistoryEntry entry =new HistoryEntry(MainSegueIdentifier.detailRestaurantSegueIdentifier,restaurantUUID);
 //        HistoryEntry entry =new HistoryEntry(MainSegueIdentifier.detailEventSegueIdentifier);
 //        HistoryEntry entry =new HistoryEntry(MainSegueIdentifier.detailOrderedRecipesSegueIdentifier);
 //        HistoryEntry entry =new HistoryEntry(MainSegueIdentifier.detailRecipeSegueIdentifier);
