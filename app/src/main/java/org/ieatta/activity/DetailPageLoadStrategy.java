bad4910ab1a06b0cebbc2ba1dfa17a56ba7d5295
Trujun Zhang <trujunzhang@gmail.com>
2016-03-25 08:51:12 +0800

    IEATTA for android.

--- app/src/main/java/org/ieatta/activity/DetailPageLoadStrategy.java
+++ app/src/main/java/org/ieatta/activity/DetailPageLoadStrategy.java
@@ -5,6 +5,7 @@ import android.content.res.Resources;
 import android.support.annotation.DimenRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import org.ieatta.IEAApp;
 import org.ieatta.activity.editing.EditHandler;
@@ -284,21 +285,21 @@ public class DetailPageLoadStrategy implements PageLoadStrategy {
     }
 
     private void displayLeadSection() {
-//        Page page = model.getPage();
-//
+        Page page = model.getPage();
+
 //        sendMarginPayload();
 //
 //        sendLeadSectionPayload(page);
 //
 //        sendMiscPayload(page);
 //
-//        if (webView.getVisibility() != View.VISIBLE) {
-//            webView.setVisibility(View.VISIBLE);
-//        }
-//
-//        refreshView.setRefreshing(false);
+        if (webView.getVisibility() != View.VISIBLE) {
+            webView.setVisibility(View.VISIBLE);
+        }
+
+        refreshView.setRefreshing(false);
 //        activity.updateProgressBar(true, true, 0);
-//
+
 //        leadImagesHandler.updateNavigate(page.getPageProperties().getGeo());
     }
 }
