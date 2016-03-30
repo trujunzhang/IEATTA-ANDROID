7c154cc31d727a2cd9b88cc4261d9dc873dd5479
Trujun Zhang <trujunzhang@gmail.com>
2016-03-30 09:12:39 +0800

    IEATTA for android.

--- app/src/main/java/org/ieatta/provide/MainSegueIdentifier.java
+++ app/src/main/java/org/ieatta/provide/MainSegueIdentifier.java
@@ -16,10 +16,10 @@ import org.ieatta.tasks.RestaurantDetailTask;
 
 public enum MainSegueIdentifier {
 
-    Unspecified(-1,""),
+    Unspecified(-1, ""),
 
     // Main fragment.
-    nearbyRestaurants(AppConstant.SOURCE_NEARBY_RESTAURANTS,"nearbyRestaurant"),
+    nearbyRestaurants(AppConstant.SOURCE_NEARBY_RESTAURANTS, "nearbyRestaurant"),
 
 //    // Four menus in the near restaurant page.
 //    searchRestaurantSegueIdentifier("searchRestaurant"),
@@ -27,10 +27,10 @@ public enum MainSegueIdentifier {
 //    readReviewsSegueIdentifier("addEditRestaurant"),
 
     // Four detail pages.
-    detailRestaurantSegueIdentifier(AppConstant.SOURCE_RESTAURANT_DETAIL,"detailRestaurant"),
-    detailEventSegueIdentifier(AppConstant.SOURCE_EVENT_DETAIL,"detailEvent"),
-    detailOrderedRecipesSegueIdentifier(AppConstant.SOURCE_ORDERED_RECIPES,"detailOrderedRecipes"),
-    detailRecipeSegueIdentifier(AppConstant.SOURCE_RECIPE_DETAIL,"detailRecipe"),
+    detailRestaurantSegueIdentifier(AppConstant.SOURCE_RESTAURANT_DETAIL, "detailRestaurant"),
+    detailEventSegueIdentifier(AppConstant.SOURCE_EVENT_DETAIL, "detailEvent"),
+    detailOrderedRecipesSegueIdentifier(AppConstant.SOURCE_ORDERED_RECIPES, "detailOrderedRecipes"),
+    detailRecipeSegueIdentifier(AppConstant.SOURCE_RECIPE_DETAIL, "detailRecipe"),
 
     // Show all posted reviews for restaurant,recipe.
 //    detailSeeReviewSegueIdentifier("seeReviewsInDetail"),
@@ -39,10 +39,10 @@ public enum MainSegueIdentifier {
 
 
     // Four new/edit model pages.(the following three, and restaurant)
-    editRestaurantSegueIdentifier(AppConstant.SOURCE_RESTAURANT_EDIT,"addEditRestaurant"),
-    editEventSegueIdentifier(AppConstant.SOURCE_EVENT_EDIT,"addEditEvent"),
-    editPeopleSegueIdentifier(AppConstant.SOURCE_PEOPLE_EDIT,"addEditPeople"),
-    editRecipeSegueIdentifier(AppConstant.SOURCE_RECIPE_EDIT,"addEditRecipe"),
+    editRestaurantSegueIdentifier(AppConstant.SOURCE_RESTAURANT_EDIT, "addEditRestaurant"),
+    editEventSegueIdentifier(AppConstant.SOURCE_EVENT_EDIT, "addEditEvent"),
+    editPeopleSegueIdentifier(AppConstant.SOURCE_PEOPLE_EDIT, "addEditPeople"),
+    editRecipeSegueIdentifier(AppConstant.SOURCE_RECIPE_EDIT, "addEditRecipe"),
 
     // Choice Person in the event page.
 //    choicePeopleSegueIdentifier("choicePeople"),
@@ -50,13 +50,13 @@ public enum MainSegueIdentifier {
 //    // Show detail review from review list.
 //    postReviewSegueIdentifier("postReview"),
 
-    photoPagesControllerSegueIdentifier(100,"photoPagesController");
+    photoPagesControllerSegueIdentifier(100, "photoPagesController");
 
 
     private final String name;
     private final int type;
 
-    MainSegueIdentifier(int type,String name) {
+    MainSegueIdentifier(int type, String name) {
         this.type = type;
         this.name = name;
     }
@@ -65,18 +65,28 @@ public enum MainSegueIdentifier {
         return type;
     }
 
-    public static FragmentTask getFragment(HistoryEntry entry, Activity activity, PageViewModel model){
-        switch (entry.getSource()){
+    public static FragmentTask getFragment(HistoryEntry entry, Activity activity, PageViewModel model) {
+        switch (entry.getSource()) {
+            // Four detail pages.
             case AppConstant.SOURCE_NEARBY_RESTAURANTS:
-                return new NearRestaurantsTask(entry, activity,model);
+                return new NearRestaurantsTask(entry, activity, model);
             case AppConstant.SOURCE_RESTAURANT_DETAIL:
-                return new RestaurantDetailTask(entry, activity,model);
+                return new RestaurantDetailTask(entry, activity, model);
             case AppConstant.SOURCE_EVENT_DETAIL:
-                return new EventDetailTask(entry, activity,model);
+                return new EventDetailTask(entry, activity, model);
             case AppConstant.SOURCE_ORDERED_RECIPES:
-                return new OrderedRecipesTask(entry, activity,model);
+                return new OrderedRecipesTask(entry, activity, model);
             case AppConstant.SOURCE_RECIPE_DETAIL:
-                return new RecipeDetailTask(entry, activity,model);
+                return new RecipeDetailTask(entry, activity, model);
+            // Four new/edit model pages.(the following three, and restaurant)
+//            case AppConstant.SOURCE_RESTAURANT_EDIT:
+//                return new XXX(entry, activity, model);
+//            case AppConstant.SOURCE_EVENT_EDIT:
+//                return new XXX(entry, activity, model);
+//            case AppConstant.SOURCE_PEOPLE_EDIT:
+//                return new XXX(entry, activity, model);
+//            case AppConstant.SOURCE_RECIPE_EDIT:
+//                return new XXX(entry, activity, model);
         }
 
         return null;
