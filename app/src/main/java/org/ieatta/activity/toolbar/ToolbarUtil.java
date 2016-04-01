f4893ce6b1c42dad9db4e91464d51af54fa5d31e
Trujun Zhang <trujunzhang@gmail.com>
2016-04-01 21:19:04 +0800

    IEATTA for android.

---
+++ app/src/main/java/org/ieatta/activity/toolbar/ToolbarUtil.java
@@ -0,0 +1,12 @@
+package org.ieatta.activity.toolbar;
+
+public class ToolbarUtil {
+    // Restaurant detail:
+    // _"Add Event","Write Review","See Reviews", _"Edit"
+    // Event detail:
+    // _"Select Person","Write Review","See Reviews" , _"Edit"
+    // Ordered recipes:
+    // _"Add Food"
+    // Recipe detail:
+    // _"Write Review","See Reviews", _"Edit"
+}
