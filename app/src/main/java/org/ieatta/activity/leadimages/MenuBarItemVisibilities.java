9c9c1f4e196d7dc667876263c7b908dfe29d1d15
Trujun Zhang <trujunzhang@gmail.com>
2016-04-02 15:19:36 +0800

    IEATTA for Android.

--- app/src/main/java/org/ieatta/activity/leadimages/MenuBarItemVisibilities.java
+++ app/src/main/java/org/ieatta/activity/leadimages/MenuBarItemVisibilities.java
@@ -6,32 +6,84 @@ import android.view.View;
 import org.ieatta.R;
 import org.ieatta.parse.AppConstant;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 public final class MenuBarItemVisibilities {
 
-    public static int[] menuBarItemsVisibility(int source) {
+    public static final int type_menu_bar_bookmark = 0;
+    public static final int type_menu_bar_share = 1;
+    public static final int type_menu_bar_navigate = 2;
+
+    public static final int type_menu_bar_write_review = 3;
+    public static final int type_menu_bar_see_reviews = 4;
+    public static final int type_menu_bar_edit = 5;
+
+    public static final int type_menu_bar_add_event = 6;
+    public static final int type_menu_bar_select_person = 7;
+    public static final int type_menu_bar_add_food = 8;
+
+    public static HashMap<Integer, Integer> menuBarItemsVisibility(int source) {
         switch (source) {
             case AppConstant.SOURCE_RESTAURANT_DETAIL:
-                return new int[]{View.VISIBLE};
+                return new LinkedHashMap<Integer,Integer>() {{
+                    put(type_menu_bar_bookmark, View.GONE);
+                    put(type_menu_bar_share, View.GONE);
+                    put(type_menu_bar_navigate, View.GONE);
+
+                    put(type_menu_bar_write_review, View.GONE);
+                    put(type_menu_bar_see_reviews, View.GONE);
+                    put(type_menu_bar_edit, View.GONE);
+
+                    put(type_menu_bar_add_event, View.GONE);
+                    put(type_menu_bar_select_person, View.GONE);
+                    put(type_menu_bar_add_food, View.GONE);
+                }};
             case AppConstant.SOURCE_EVENT_DETAIL:
-                break;
+                return new LinkedHashMap<Integer,Integer>() {{
+                    put(type_menu_bar_bookmark, View.GONE);
+                    put(type_menu_bar_share, View.GONE);
+                    put(type_menu_bar_navigate, View.GONE);
+
+                    put(type_menu_bar_write_review, View.GONE);
+                    put(type_menu_bar_see_reviews, View.GONE);
+                    put(type_menu_bar_edit, View.GONE);
+
+                    put(type_menu_bar_add_event, View.GONE);
+                    put(type_menu_bar_select_person, View.GONE);
+                    put(type_menu_bar_add_food, View.GONE);
+                }};
             case AppConstant.SOURCE_ORDERED_RECIPES:
-                break;
+                return new LinkedHashMap<Integer,Integer>() {{
+                    put(type_menu_bar_bookmark, View.GONE);
+                    put(type_menu_bar_share, View.GONE);
+                    put(type_menu_bar_navigate, View.GONE);
+
+                    put(type_menu_bar_write_review, View.GONE);
+                    put(type_menu_bar_see_reviews, View.GONE);
+                    put(type_menu_bar_edit, View.GONE);
+
+                    put(type_menu_bar_add_event, View.GONE);
+                    put(type_menu_bar_select_person, View.GONE);
+                    put(type_menu_bar_add_food, View.GONE);
+                }};
             case AppConstant.SOURCE_RECIPE_DETAIL:
-                break;
+                return new LinkedHashMap<Integer,Integer>() {{
+                    put(type_menu_bar_bookmark, View.GONE);
+                    put(type_menu_bar_share, View.GONE);
+                    put(type_menu_bar_navigate, View.GONE);
+
+                    put(type_menu_bar_write_review, View.GONE);
+                    put(type_menu_bar_see_reviews, View.GONE);
+                    put(type_menu_bar_edit, View.GONE);
+
+                    put(type_menu_bar_add_event, View.GONE);
+                    put(type_menu_bar_select_person, View.GONE);
+                    put(type_menu_bar_add_food, View.GONE);
+                }};
         }
         return null;
     }
 
-//            R.id.view_article_menu_bar_bookmark,
-//            R.id.view_article_menu_bar_share,
-//            R.id.view_article_menu_bar_navigate,
-//
-//            R.id.view_article_menu_bar_write_review,
-//            R.id.view_article_menu_bar_see_reviews,
-//            R.id.view_article_menu_bar_edit,
-//
-//            R.id.view_article_menu_bar_add_event,
-//            R.id.view_article_menu_bar_select_person,
-//            R.id.view_article_menu_bar_add_food,
 
 }
