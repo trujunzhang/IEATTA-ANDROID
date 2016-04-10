56edbe0273b558cc0b0e9f56e159181fb29b5501
Trujun Zhang <trujunzhang@gmail.com>
2016-04-10 09:40:34 +0800

    IEATTA for android.

--- app/src/main/java/org/ieatta/activity/dialog/contacts/ContactsDialog.java
+++ app/src/main/java/org/ieatta/activity/dialog/contacts/ContactsDialog.java
@@ -2,13 +2,25 @@ package org.ieatta.activity.dialog.contacts;
 
 
 import android.content.DialogInterface;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.ieatta.activity.dialog.SwipeableBottomDialog;
 
-public class ContactsDialog  extends SwipeableBottomDialog implements DialogInterface.OnDismissListener{
+public class ContactsDialog extends SwipeableBottomDialog implements DialogInterface.OnDismissListener {
+
+    public static ContactsDialog newInstance(int entrySource) {
+        ContactsDialog dialog = new ContactsDialog();
+        Bundle args = new Bundle();
+        // args.putParcelable("title", title);
+        // args.putInt("entrySource", entrySource);
+        dialog.setArguments(args);
+        return dialog;
+    }
+
+
     @Override
     protected View inflateDialogView(LayoutInflater inflater, ViewGroup container) {
         return null;
