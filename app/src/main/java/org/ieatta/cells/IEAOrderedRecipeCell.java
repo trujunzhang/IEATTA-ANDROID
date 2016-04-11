c2d321d87de5569d7b249a5e0ac6915e7f4e2be9
Trujun Zhang <trujunzhang@gmail.com>
2016-04-11 08:12:24 +0800

    IEATTA for android.

--- app/src/main/java/org/ieatta/cells/IEAOrderedRecipeCell.java
+++ app/src/main/java/org/ieatta/cells/IEAOrderedRecipeCell.java
@@ -10,6 +10,7 @@ import org.ieatta.R;
 import org.ieatta.database.models.DBRecipe;
 import org.ieatta.views.AvatarView;
 import org.ieatta.views.RatingImageView;
+import org.wikipedia.views.GoneIfEmptyTextView;
 
 public class IEAOrderedRecipeCell extends IEAViewHolder {
     public static CellType getType() {
@@ -19,15 +20,15 @@ public class IEAOrderedRecipeCell extends IEAViewHolder {
     private AvatarView avatarView;
 
     private TextView displayNameLabel;
-    private TextView priceLabel;
+    private GoneIfEmptyTextView priceLabel;
     private RatingImageView ratingImageView;
 
     public IEAOrderedRecipeCell(View itemView) {
         super(itemView);
 
-        this.avatarView = (AvatarView) itemView.findViewById(R.id.avatarView);
-        this.displayNameLabel = (TextView) itemView.findViewById(R.id.titleTextView);
-        this.priceLabel = (TextView) itemView.findViewById(R.id.priceTextView);
+        this.avatarView = (AvatarView) itemView.findViewById(R.id.page_list_item_image);
+        this.displayNameLabel = (TextView) itemView.findViewById(R.id.page_list_item_title);
+        this.priceLabel = (GoneIfEmptyTextView) itemView.findViewById(R.id.page_list_item_description);
         this.ratingImageView = (RatingImageView) itemView.findViewById(R.id.business_review_star_rating);
     }
 
