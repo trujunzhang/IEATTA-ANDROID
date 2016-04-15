402893d42c4eac4f794ac1945924c9b69978b567
Trujun Zhang <trujunzhang@gmail.com>
2016-04-15 10:07:09 +0800

    IEATTA for android.

--- app/src/main/java/org/ieatta/tasks/RecipeDetailTask.java
+++ app/src/main/java/org/ieatta/tasks/RecipeDetailTask.java
@@ -8,6 +8,7 @@ import com.tableview.adapter.NSIndexPath;
 
 import org.ieatta.activity.LeadImageCollection;
 import org.ieatta.activity.Page;
+import org.ieatta.activity.PageActivity;
 import org.ieatta.activity.PageProperties;
 import org.ieatta.activity.PageTitle;
 import org.ieatta.activity.PageViewModel;
@@ -30,6 +31,7 @@ import org.ieatta.database.provide.ReviewType;
 import org.ieatta.database.query.LocalDatabaseQuery;
 import org.ieatta.database.realm.RealmModelReader;
 import org.ieatta.parse.AppConstant;
+import org.ieatta.provide.MainSegueIdentifier;
 import org.ieatta.server.cache.ThumbnailImageUtil;
 
 import java.io.File;
@@ -40,10 +42,10 @@ import bolts.Task;
 import io.realm.RealmResults;
 
 public class RecipeDetailTask extends FragmentTask {
-    public DBRestaurant restaurant;
-    public DBEvent event;
-    public DBTeam team;
-    public DBRecipe recipe;
+    public DBRestaurant mRestaurant;
+    public DBEvent mEvent;
+    public DBTeam mTeam;
+    public DBRecipe mRecipe;
 
     private LeadImageCollection leadImageCollection; // for recipes
 
@@ -77,33 +79,29 @@ public class RecipeDetailTask extends FragmentTask {
         return new RealmModelReader<DBRecipe>(DBRecipe.class).getFirstObject(LocalDatabaseQuery.get(_recipeUUID), false, realmList).onSuccessTask(new Continuation<DBRecipe, Task<DBTeam>>() {
             @Override
             public Task<DBTeam> then(Task<DBRecipe> task) throws Exception {
-                DBRecipe recipe = task.getResult();
-                RecipeDetailTask.this.mRecipeUUID = recipe.getUUID();
-                RecipeDetailTask.this.mTeamUUID = recipe.getOrderedPeopleRef();
-                RecipeDetailTask.this.mEventUUID = recipe.getEventRef();
-                RecipeDetailTask.this.recipe = recipe;
+                RecipeDetailTask.this.mRecipeUUID = task.getResult().getUUID();
+                RecipeDetailTask.this.mTeamUUID = task.getResult().getOrderedPeopleRef();
+                RecipeDetailTask.this.mEventUUID = task.getResult().getEventRef();
+                RecipeDetailTask.this.mRecipe = task.getResult();
                 return new RealmModelReader<DBTeam>(DBTeam.class).getFirstObject(LocalDatabaseQuery.get(RecipeDetailTask.this.mTeamUUID), false, realmList);
             }
         }).onSuccessTask(new Continuation<DBTeam, Task<DBEvent>>() {
             @Override
             public Task<DBEvent> then(Task<DBTeam> task) throws Exception {
-                DBTeam team = task.getResult();
-                RecipeDetailTask.this.team = team;
+                RecipeDetailTask.this.mTeam = task.getResult();
                 return new RealmModelReader<DBEvent>(DBEvent.class).getFirstObject(LocalDatabaseQuery.get(RecipeDetailTask.this.mEventUUID), false, realmList);
             }
         }).onSuccessTask(new Continuation<DBEvent, Task<DBRestaurant>>() {
             @Override
             public Task<DBRestaurant> then(Task<DBEvent> task) throws Exception {
-                DBEvent event = task.getResult();
-                RecipeDetailTask.this.event = event;
-                RecipeDetailTask.this.mRestaurantUUID = event.getRestaurantRef();
+                RecipeDetailTask.this.mEvent = task.getResult();
+                RecipeDetailTask.this.mRestaurantUUID = task.getResult().getRestaurantRef();
                 return new RealmModelReader<DBRestaurant>(DBRestaurant.class).getFirstObject(LocalDatabaseQuery.get(RecipeDetailTask.this.mRestaurantUUID), false, realmList);
             }
         }).onSuccessTask(new Continuation<DBRestaurant, Task<RealmResults<DBPhoto>>>() {
             @Override
             public Task<RealmResults<DBPhoto>> then(Task<DBRestaurant> task) throws Exception {
-                DBRestaurant restaurant = task.getResult();
-                RecipeDetailTask.this.restaurant = restaurant;
+                RecipeDetailTask.this.mRestaurant = task.getResult();
                 return LocalDatabaseQuery.queryPhotosByModel(_recipeUUID, PhotoUsedType.PU_Recipe.getType(), realmList);
             }
         }).onSuccessTask(new Continuation<RealmResults<DBPhoto>, Task<List<File>>>() {
@@ -153,11 +151,21 @@ public class RecipeDetailTask extends FragmentTask {
     }
 
     public Page getPage() {
-        String title = recipe.getDisplayName();
-        String description = String.format("$ %s", recipe.getPrice());
-        PageTitle pageTitle = new PageTitle(this.recipe.getUUID(),null,description, reviewQuery.ratingReview);
+        String title = this.mRecipe.getDisplayName();
+        String description = String.format("$ %s", this.mRecipe.getPrice());
+        PageTitle pageTitle = new PageTitle(this.mRecipe.getUUID(),null,description, reviewQuery.ratingReview);
         PageProperties properties = new PageProperties(this.leadImageCollection, title,null);
 
         return new Page(pageTitle, properties);
+    }
+
+
+    @Override
+    /**
+     * Edit the detailed restaurant.
+     */
+    public void onEditClick() {
+        ((PageActivity) activity).loadPage(
+                new HistoryEntry(MainSegueIdentifier.editRecipeSegueIdentifier, this.mRecipe.getUUID(),false));
     }
 }
